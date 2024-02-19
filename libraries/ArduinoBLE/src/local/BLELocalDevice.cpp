4fca16b89f37c34b3f052705fa2c6e34e88a60ec
Raina Matthews <rmatthews@forteholdings.com>
2024-02-19 12:14:14 -0800

    Updating some libraries

--- libraries/ArduinoBLE/src/local/BLELocalDevice.cpp
+++ libraries/ArduinoBLE/src/local/BLELocalDevice.cpp
@@ -25,9 +25,17 @@
 
 #include "BLELocalDevice.h"
 
-#if defined(ARDUINO_PORTENTA_H7_M4) || defined(ARDUINO_PORTENTA_H7_M7)
+#if defined(ARDUINO_PORTENTA_H7_M4) || defined(ARDUINO_PORTENTA_H7_M7) || defined(ARDUINO_OPTA)
 #ifndef BT_REG_ON
 #define BT_REG_ON PJ_12
+#endif
+#elif defined(ARDUINO_NICLA_VISION)
+#ifndef BT_REG_ON
+#define BT_REG_ON PF_14
+#endif
+#elif defined(ARDUINO_GIGA)
+#ifndef BT_REG_ON
+#define BT_REG_ON PA_10
 #endif
 #endif
 
@@ -61,10 +69,30 @@ int BLELocalDevice::begin()
   delay(100);
   digitalWrite(NINA_RESETN, HIGH);
   delay(750);
-#elif defined(ARDUINO_PORTENTA_H7_M4) || defined(ARDUINO_PORTENTA_H7_M7)
+#elif defined(ARDUINO_PORTENTA_H7_M4) || defined(ARDUINO_PORTENTA_H7_M7) || defined(ARDUINO_NICLA_VISION) || defined(ARDUINO_GIGA) || defined(ARDUINO_OPTA)
   // BT_REG_ON -> HIGH
   pinMode(BT_REG_ON, OUTPUT);
   digitalWrite(BT_REG_ON, HIGH);
+#elif defined(ARDUINO_PORTENTA_C33)
+#define NINA_GPIO0      (100)
+#define NINA_RESETN     (101)
+  pinMode(NINA_GPIO0, OUTPUT);
+  pinMode(NINA_RESETN, OUTPUT);
+  Serial5.begin(921600);
+
+  digitalWrite(NINA_GPIO0, HIGH);
+  delay(100);
+  digitalWrite(NINA_RESETN, HIGH);
+  digitalWrite(NINA_RESETN, LOW);
+  digitalWrite(NINA_RESETN, HIGH);
+  auto _start = millis();
+  while (millis() - _start < 500) {
+    if (Serial5.available()) {
+      Serial5.read();
+    }
+  }
+  //pinMode(94, OUTPUT);
+  //digitalWrite(94, LOW);
 #endif
 
 
@@ -108,6 +136,10 @@ int BLELocalDevice::begin()
     end();
     return 0;
   }
+  if (HCI.setLeEventMask(0x00000000000003FF) != 0) {
+    end();
+    return 0;
+  }
 
   uint16_t pktLen;
   uint8_t maxPkt;
@@ -116,6 +148,59 @@ int BLELocalDevice::begin()
     end();
     return 0;
   }
+
+  /// The HCI should allow automatic address resolution.
+
+  // // If we have callbacks to remember bonded devices:
+  // if(HCI._getIRKs!=0){
+  //   uint8_t nIRKs = 0;
+  //   uint8_t** BADDR_Type = new uint8_t*;
+  //   uint8_t*** BADDRs = new uint8_t**;
+  //   uint8_t*** IRKs = new uint8_t**;
+  //   uint8_t* memcheck;
+
+
+  //   if(!HCI._getIRKs(&nIRKs, BADDR_Type, BADDRs, IRKs)){
+  //     Serial.println("error");
+  //   }
+  //   for(int i=0; i<nIRKs; i++){
+  //     Serial.print("Baddr type: ");
+  //     Serial.println((*BADDR_Type)[i]);
+  //     Serial.print("BADDR:");
+  //     for(int k=0; k<6; k++){
+  //       Serial.print(", 0x");
+  //       Serial.print((*BADDRs)[i][k],HEX);
+  //     }
+  //     Serial.println();
+  //     Serial.print("IRK:");
+  //     for(int k=0; k<16; k++){
+  //       Serial.print(", 0x");
+  //       Serial.print((*IRKs)[i][k],HEX);
+  //     }
+  //     Serial.println();
+
+  //     // save this 
+  //     uint8_t zeros[16];
+  //     for(int k=0; k<16; k++) zeros[15-k] = 0;
+      
+  //     // HCI.leAddResolvingAddress((*BADDR_Type)[i],(*BADDRs)[i],(*IRKs)[i], zeros);
+
+  //     delete[] (*BADDRs)[i];
+  //     delete[] (*IRKs)[i];
+  //   }
+  //   delete[] (*BADDR_Type);
+  //   delete BADDR_Type;
+  //   delete[] (*BADDRs);
+  //   delete BADDRs;
+  //   delete[] (*IRKs);
+  //   delete IRKs;
+    
+  //   memcheck = new uint8_t[1];
+  //   Serial.print("nIRK location: 0x");
+  //   Serial.println((int)memcheck,HEX);
+  //   delete[] memcheck;
+
+  // }
 
   GATT.begin();
 
@@ -134,8 +219,7 @@ void BLELocalDevice::end()
 #elif defined(ARDUINO_SAMD_NANO_33_IOT) || defined(ARDUINO_NANO_RP2040_CONNECT)
   // disable the NINA
   digitalWrite(NINA_RESETN, LOW);
-#elif defined(ARDUINO_PORTENTA_H7_M4) || defined(ARDUINO_PORTENTA_H7_M7)
-  // BT_REG_ON -> LOW
+#elif defined(ARDUINO_PORTENTA_H7_M4) || defined(ARDUINO_PORTENTA_H7_M7) || defined(ARDUINO_NICLA_VISION) || defined(ARDUINO_GIGA) || defined(ARDUINO_OPTA)
   digitalWrite(BT_REG_ON, LOW);
 #endif
 }
@@ -155,6 +239,16 @@ bool BLELocalDevice::connected() const
   HCI.poll();
 
   return ATT.connected();
+}
+
+/*
+ * Whether there is at least one paired device
+ */
+bool BLELocalDevice::paired()
+{
+  HCI.poll();
+
+  return ATT.paired();
 }
 
 bool BLELocalDevice::disconnect()
@@ -336,6 +430,42 @@ void BLELocalDevice::setConnectable(bool connectable)
 void BLELocalDevice::setTimeout(unsigned long timeout)
 {
   ATT.setTimeout(timeout);
+}
+
+/*
+ * Control whether pairing is allowed or rejected
+ * Use true/false or the Pairable enum
+ */
+void BLELocalDevice::setPairable(uint8_t pairable)
+{
+  L2CAPSignaling.setPairingEnabled(pairable);
+}
+
+/*
+ * Whether pairing is currently allowed
+ */
+bool BLELocalDevice::pairable()
+{
+  return L2CAPSignaling.isPairingEnabled();
+}
+
+void BLELocalDevice::setGetIRKs(int (*getIRKs)(uint8_t* nIRKs, uint8_t** BADDR_type, uint8_t*** BADDRs, uint8_t*** IRKs)){
+  HCI._getIRKs = getIRKs;
+}
+void BLELocalDevice::setGetLTK(int (*getLTK)(uint8_t* BADDR, uint8_t* LTK)){
+  HCI._getLTK = getLTK;
+}
+void BLELocalDevice::setStoreLTK(int (*storeLTK)(uint8_t*, uint8_t*)){
+  HCI._storeLTK = storeLTK;
+}
+void BLELocalDevice::setStoreIRK(int (*storeIRK)(uint8_t*, uint8_t*)){
+  HCI._storeIRK = storeIRK;
+}
+void BLELocalDevice::setDisplayCode(void (*displayCode)(uint32_t confirmationCode)){
+  HCI._displayCode = displayCode;
+}
+void BLELocalDevice::setBinaryConfirmPairing(bool (*binaryConfirmPairing)()){
+  HCI._binaryConfirmPairing = binaryConfirmPairing;
 }
 
 void BLELocalDevice::debug(Stream& stream)
--- libraries/ArduinoBLE/src/utility/HCIUartTransport.cpp
+++ libraries/ArduinoBLE/src/utility/HCIUartTransport.cpp
@@ -17,7 +17,7 @@
   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#if !defined(ARDUINO_ARCH_MBED) || defined(TARGET_NANO_RP2040_CONNECT)
+#if !defined(ARDUINO_ARCH_MBED) && !defined(ESP32) && !defined(ARDUINO_UNOR4_WIFI) || defined(TARGET_NANO_RP2040_CONNECT)
 
 #include "HCIUartTransport.h"
 
@@ -27,8 +27,12 @@
 // SerialHCI is already defined in the variant
 #elif defined(ARDUINO_PORTENTA_H7_M4)
 // SerialHCI is already defined in the variant
-#elif defined(ARDUINO_PORTENTA_H7_M7)
+#elif defined(ARDUINO_PORTENTA_H7_M7) || defined(ARDUINO_NICLA_VISION)
 #define SerialHCI Serial2
+#elif defined(ARDUINO_OPTA)
+#define SerialHCI Serial3
+#elif defined(ARDUINO_PORTENTA_C33)
+#define SerialHCI Serial5
 #else
 #error "Unsupported board selected!"
 #endif
